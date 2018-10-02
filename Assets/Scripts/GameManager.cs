56fa91b60551d7927435382d0c35b218e38f5ffa
ChiefTripodBear <avalacos@gmail.com>
2018-10-02 22:55:08 +1000

    Implemented Waypoint System
    There's now a waypoint system (i've only put it in level 1 so far but it should be easy to input on other levels).
    
    I still need to disable controls while intro waypoint cutscene is active.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    #region singleton setup
+    private static GameManager instance;
+
+    public static GameManager Instance
+    {
+        get
+        {
+            return instance;
+        }
+
+        set
+        {
+            instance = value;
+        }
+    }
+
+    #endregion
+
+    #region 
+    public bool introIsComplete = false;
+
+    #endregion
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        instance = this;
+    }
+
+    private void Update()
+    {
+        Debug.Log(introIsComplete);
+    }
+}
---
+++ Assets/Scripts/IntroWaypointFollower.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class IntroWaypointFollower : MonoBehaviour
+{
+    public GameObject[] waypoints; //array of waypoints to cycle through during intro
+    int currentWP = 0;
+
+    public float introCamSpeed = 15f;     //control camera speed during intro   
+    public float introCamAngularSpeed = 4f;  //control turning speed of camera during intro
+    public float introCamAccuracy = 4f;  //control accuracy of camera to waypoint
+
+    private Vector3 direction;          //the vector direction for the next waypoint
+
+    void LateUpdate ()
+    {
+        if (!GameManager.Instance.introIsComplete)
+        {
+            //if there are no waypoints, return
+            if (waypoints.Length == 0)
+            {
+                GameManager.Instance.introIsComplete = true;
+            }
+
+            if (!GameManager.Instance.introIsComplete)
+            {
+                direction = waypoints[currentWP].transform.position - this.transform.position;
+                this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), Time.deltaTime * introCamAngularSpeed);
+            }
+
+            if (direction.magnitude < introCamAccuracy) //array still has waypoints to cycle through
+            {
+                currentWP++;
+                if (currentWP >= waypoints.Length)
+                {
+                    GameManager.Instance.introIsComplete = true;
+                }
+            }
+            //move to waypoint
+            this.transform.position += transform.forward * introCamSpeed * Time.deltaTime;
+
+        }
+    }
+}   
