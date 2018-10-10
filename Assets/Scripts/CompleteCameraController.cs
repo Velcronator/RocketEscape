6e921ef6c01f1d86540ca9bc3d5b18e96fbd760f
ChiefTripodBear <avalacos@gmail.com>
2018-10-10 13:47:07 +1100

    Revert "Removed Waypoints, Collider Script for occluding"
    This reverts commit da4fa5318aea8c09c1f49dca70457433ca7a6e8f.

--- Assets/Scripts/CompleteCameraController.cs
+++ Assets/Scripts/CompleteCameraController.cs
@@ -1,25 +1,45 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 public class CompleteCameraController : MonoBehaviour
 {
-
     public GameObject player;       //Public variable to store a reference to the player game object
+    public GameObject wpFollower;   //reference to the waypoint follower, to retrieve the status of the intro completion bool
 
-
+    private Transform target;       //reference to the camera's target (player or waypoint follower)
     private Vector3 offset;         //Private variable to store the offset distance between the player and camera
 
-    // Use this for initialization
-    void Start()
+    void Awake()
     {
         //Calculate and store the offset value by getting the distance between the player's position and camera's position.
         offset = transform.position - player.transform.position;
     }
 
+    private void Update()
+    {
+        //as long as the intro isn't complete, camera should focus on waypoint follower.
+        //once intro is complete, camera should follow player
+
+        //a reference to the transform of the camera target
+
+        switch (GameManager.Instance.introIsComplete)
+        {
+            case true:
+                target = player.transform;
+                break;
+            case false:
+                target = wpFollower.transform;
+                break;
+
+            default:
+                target = player.transform;
+                break;
+        }
+    }
+
     // LateUpdate is called after Update each frame
     void LateUpdate()
     {
-        // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
-        transform.position = player.transform.position + offset;
+        transform.position = target.position + offset;
+
     }
 }
